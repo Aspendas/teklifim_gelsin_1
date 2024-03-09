acf116e1eaf38a69cde62f91dd6f5e80b85a7b97
Umut Y. Çalışkan <umutyasin2001@gmail.com>
2024-03-10 00:40:53 +0300

    add methods to get Active Passive Sponsored offers and get the total number of loans

--- lib/viewmodel/loan.dart
+++ lib/viewmodel/loan.dart
@@ -1,5 +1,6 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -11,49 +12,43 @@ class LoanViewModel extends ChangeNotifier {
 
   List<Loan> get loans => _loans;
 
-  LoanViewModel() {
-    fetchLoans();
-  }
-
-  Future<void> fetchLoans() async {
-    final response = await http.get(Uri.parse(
-        'https://api2.teklifimgelsin.com/api/getLoanOffers?kredi_tipi=0&vade=22&tutar=65000'));
+  Future<void> fetchLoans(String maturity, String amount,
+      {int type = 0}) async {
+    log('fetchLoans: $maturity, $amount, $type');
+    final response = await http.get(
+      Uri.parse(
+          'https://api2.teklifimgelsin.com/api/getLoanOffers?kredi_tipi=$type&vade=$maturity&tutar=$amount'),
+    );
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);
       if (jsonData is Map<String, dynamic>) {
-        // Handle the JSON data as a Map
         _loans = [Loan.fromJson(jsonData)];
       } else if (jsonData is List) {
-        // Handle the JSON data as a List
         _loans = jsonData.map((data) => Loan.fromJson(data)).toList();
       } else {
         _loans = [];
       }
-      print(_loans.toString() + "xxxxxxx");
       notifyListeners();
-    } else {
-      throw Exception('Failed to load loans');
     }
   }
 
-  Future<void> fetchLoans2() async {
-    final response = await http.get(Uri.parse(
-        'https://api2.teklifimgelsin.com/api/getLoanOffers?kredi_tipi=0&vade=5&tutar=54000'));
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      if (jsonData is Map<String, dynamic>) {
-        // Handle the JSON data as a Map
-        _loans = [Loan.fromJson(jsonData)];
-      } else if (jsonData is List) {
-        // Handle the JSON data as a List
-        _loans = jsonData.map((data) => Loan.fromJson(data)).toList();
-      } else {
-        _loans = [];
-      }
-      print("2");
-      notifyListeners();
-    } else {
-      throw Exception('Failed to load loans');
-    }
+  getLoanNumber() {
+    int count = 0;
+    count = (_loans.first.activeOffers?.length ?? 0) +
+        (_loans.first.passiveOffers?.length ?? 0) +
+        (_loans.first.sponsoredOffers?.length ?? 0);
+    return count;
+  }
+
+  getActiveOffers() {
+    return _loans.first.activeOffers;
+  }
+
+  getPassiveOffers() {
+    return _loans.first.passiveOffers;
+  }
+
+  getSponsoredOffers() {
+    return _loans.first.sponsoredOffers;
   }
 }
